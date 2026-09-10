cfc76dc68effe6611c860889d88cfd29a93c591c
Okonkwoisrael2023 <147055825+Okonkwoisrael2023@users.noreply.github.com>
2026-09-10 09:47:21 -0700

    Add files via upload
    can save to local storage now

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const display = document.getElementById("inputbox");
 
+// Load the last result when the page opens
+const lastResult = localStorage.getItem("lastResult");
+
+if (lastResult !== null) {
+    display.value = lastResult;
+}
+
+
+// Calculate
+function calculate() {
+
+    const calculation = display.value;
+
+    if (calculation === "") {
+        return;
+    }
+
+    const result = eval(calculation);
+
+    // Show result
+    display.value = result;
+
+    // Save result to localStorage
+    localStorage.setItem("lastResult", result);
+};
+
+function clearCalculator() {
+    display.value = "";
+    localStorage.removeItem("lastResult");
+};
