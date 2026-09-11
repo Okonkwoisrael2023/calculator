9d949e3033505e893185a7cd61c987c24270c965
Okonkwoisrael2023 <147055825+Okonkwoisrael2023@users.noreply.github.com>
2026-09-11 12:03:37 -0700

    Add files via upload
    fixxed some few bug

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 const display = document.getElementById("inputbox");
 
-// Load the last result when the page opens
-const lastResult = localStorage.getItem("lastResult");
-
-if (lastResult !== null) {
-    display.value = lastResult;
+// Restore last calculated answer on refresh
+const savedValue = localStorage.getItem("lastAnswer");
+if (savedValue) {
+    display.innerText = savedValue;
 }
 
+let buttons = Array.from(document.getElementsByTagName("button"));
 
-// Calculate
-function calculate() {
-
-    const calculation = display.value;
-
-    if (calculation === "") {
-        return;
-    }
-
-    const result = eval(calculation);
-
-    // Show result
-    display.value = result;
-
-    // Save result to localStorage
-    localStorage.setItem("lastResult", result);
-};
-
-function clearCalculator() {
-    display.value = "";
-    localStorage.removeItem("lastResult");
-};
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+       switch(e.target.innerText){
+        case 'DEL':
+            if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+            }
+            break;
+        case 'AC':
+            display.innerText = '';
+            localStorage.removeItem("lastAnswer");
+            break;
+        case '=':
+             try{
+                const result = eval(display.innerText);
+                display.innerText = result;
+                localStorage.setItem("lastAnswer", result);
+             } catch {
+                display.innerText = 'Error!';
+             }
+             break;
+        default:
+            display.innerText += e.target.innerText;
+       }
+    });
+});
